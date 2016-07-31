2d77e5cc20a9df6b69713fe6403d965fde253aba
kangqiao <kangqiao610@gmail.com>
2016-07-31 22:55:35 +0800

    market architecture

--- app/src/main/java/com/jzzq/broker/ui/market/MainMarketInfoFragment.java
+++ app/src/main/java/com/jzzq/broker/ui/market/MainMarketInfoFragment.java
@@ -72,43 +72,48 @@ public class MainMarketInfoFragment extends BaseFragment {
 
     @Override
     protected void onBindView(Bundle savedInstanceState) {
-                /*这是Activity的onCreate方法,有两处地方需要关注的
-                1. setSupportActionBar() 方法,告诉AppCompatActivity哪一个是ActionBar(实际是Toolbar).
-                   不然的话,做透明Status Bar(电池,手机信号那一区域)效果时候,ActionBar会位置不正确.
 
-                2. Palette, 调色板的意思,也是Android Support Library提供的.用来抓取Bitmap的颜色.在此处的用处是,
-                   当ActionBar被收折后,背景颜色能保持和Banner大图的色调一致,而Title文字的颜色保证和Banner大图的色调形成强对比.
-                */
-        final CollapsingToolbarLayout collapsingToolbar = $(R.id.collapsingToolbarLayout);
-        collapsingToolbar.setTitle(getString(R.string.app_name));
+    }
+}
+/**
+
+ 这是Activity的onCreate方法,有两处地方需要关注的
+ 1. setSupportActionBar() 方法,告诉AppCompatActivity哪一个是ActionBar(实际是Toolbar).
+ 不然的话,做透明Status Bar(电池,手机信号那一区域)效果时候,ActionBar会位置不正确.
+
+ 2. Palette, 调色板的意思,也是Android Support Library提供的.用来抓取Bitmap的颜色.在此处的用处是,
+ 当ActionBar被收折后,背景颜色能保持和Banner大图的色调一致,而Title文字的颜色保证和Banner大图的色调形成强对比.
+ *
+final CollapsingToolbarLayout collapsingToolbar = $(R.id.collapsingToolbarLayout);
+collapsingToolbar.setTitle(getString(R.string.app_name));
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.bg_splash);
         Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-            @Override
-            public void onGenerated(final Palette palette) {
-                int defaultColor = getResources().getColor(R.color.colorPrimary);
-                int defaultTitleColor = getResources().getColor(R.color.text_color_white);
-                int bgColor = palette.getDarkVibrantColor(defaultColor);
-                int titleColor = palette.getLightVibrantColor(defaultTitleColor);
-                collapsingToolbar.setContentScrimColor(bgColor);
-                collapsingToolbar.setCollapsedTitleTextColor(titleColor);
-                collapsingToolbar.setExpandedTitleColor(titleColor);
+@Override
+public void onGenerated(final Palette palette) {
+        int defaultColor = getResources().getColor(R.color.colorPrimary);
+        int defaultTitleColor = getResources().getColor(R.color.text_color_white);
+        int bgColor = palette.getDarkVibrantColor(defaultColor);
+        int titleColor = palette.getLightVibrantColor(defaultTitleColor);
+        collapsingToolbar.setContentScrimColor(bgColor);
+        collapsingToolbar.setCollapsedTitleTextColor(titleColor);
+        collapsingToolbar.setExpandedTitleColor(titleColor);
 
-                /*Palette这个类能提取以下突出的颜色：
+                Palette这个类能提取以下突出的颜色：
                 Vibrant(充满活力的)
                 Vibrant dark (充满活力的黑)
                 Vibrant light(充满活力的亮)
                 Muted(柔和的)
                 Muted dark (柔和的黑)
                 Muted lighr(柔和的亮)
-                */
-                /*Palette.Swatch vibrant = palette.getVibrantSwatch();
+
+                Palette.Swatch vibrant = palette.getVibrantSwatch();
                 if (swatch != null) {
                     // If we have a vibrant color
                     // update the title TextView
                     titleView.setBackgroundColor( vibrant.getRgb());
                     titleView.setTextColor(vibrant.getTitleTextColor());
-                }*/
-            }
+                }
+        }
         });
-    }
-}
+
+ */
