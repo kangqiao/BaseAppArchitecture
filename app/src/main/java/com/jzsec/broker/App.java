8ef06c0039a0366dc8803124232f4f14bf3ea580
zhaopan <zhaopan@zpMac.local>
2017-08-08 15:31:54 +0800

    优化项目结构

--- app/src/main/java/com/jzsec/broker/App.java
+++ app/src/main/java/com/jzsec/broker/App.java
@@ -11,7 +11,6 @@ import com.jzsec.broker.data.entity.CommonParam;
 import com.jzsec.broker.okgo.InitOkGo;
 import com.jzsec.broker.utils.SpUtil;
 import com.jzsec.broker.weex.ImageAdapter;
-import com.jzsec.leancloud.LeanCloudSetting;
 import com.taobao.weex.InitConfig;
 import com.taobao.weex.WXSDKEngine;
 
@@ -44,7 +43,7 @@ public class App extends Application /*implements RouterCallbackProvider*/ {
         initWeex();
         initialize();
         InitOkGo.onCreate(this);
-        //LeanCloudSetting.onCreate(this);
+        //com.jzsec.leancloud.LeanCloudSetting.onCreate(this);
         //初始化VCamera => http://www.jianshu.com/p/5a173841a828
         cn.kq.wxrecord.WXRecordInitializer.onCreate(this);
     }
