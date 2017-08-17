c70dab957e1a40dbe63324a454ff2710bc84a188
zhaopan <zhaopan@zpMac.local>
2017-08-17 17:25:03 +0800

    optimize tool HandlerTimer.

--- app/src/main/java/com/jzsec/broker/utils/HandlerTimer.java
+++ app/src/main/java/com/jzsec/broker/utils/HandlerTimer.java
@@ -12,6 +12,7 @@ public class HandlerTimer implements Runnable{
     public static int DEFAULT_TIMER_DELAY = 5000;
     public Handler mHandler;
     public Runnable mTask;
+    public boolean mImmediately = true;
     public int mDelay = DEFAULT_TIMER_DELAY;
 
     public HandlerTimer(Handler handler){
@@ -33,7 +34,20 @@ public class HandlerTimer implements Runnable{
     }
 
     public void startTimer(){
-        this.run();
+        startTimer(mImmediately);
+    }
+
+    public void startTimer(boolean immediately){
+        mImmediately = immediately;
+        if(mImmediately){
+            this.run();
+        } else {
+            mHandler.postDelayed(this, mDelay);
+        }
+    }
+
+    public void startTimerDelay(){
+        startTimer(false);
     }
 
     public void stopTimer(){
